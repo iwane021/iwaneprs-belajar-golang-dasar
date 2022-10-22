96f8a416fb435d3685ec09b7b968cd383937bd86
Iwan Prasetiyo <iwan.webdeveloper@gmail.com>
2022-10-23 02:29:37 +0700

    first commit

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Iwan")
+	fmt.Println("Iwan Prasetiyo")
+
+	fmt.Println(len("Iwan Prasetiyo"))
+	fmt.Println("Iwan Prasetiyo"[0])
+	fmt.Println("Iwan Prasetiyo"[1])
+}
---
+++ variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Iwan Prasetiyo"
+	fmt.Println(name)
+
+	name = "Muazzam Ahsan Sakhi"
+	fmt.Println(name)
+}
